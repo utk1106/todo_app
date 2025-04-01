0208787e9846eebb200fe05be1d10e4453b09c70
Utkarsh <thaokarutkarsh@gmail.com>
2025-04-01 16:27:30 +0530

    Updated templates and settings, added static files

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -148,6 +148,9 @@ LOGIN_URL = 'login'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where static files will be collected
+STATICFILES_DIRS = [
+    BASE_DIR / "base/static",  # This points to your app's static directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
